c2dcd3f76ab701cb3c00b5b32f889fbd58fa8b06
matinmac100 <matinmac100@yahoo.co.uk>
2020-09-13 22:22:55 +0100

    final commit 3

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -5,7 +5,6 @@ def get_pet_shop_name(pet_shop_dic):
 def get_total_cash(pet_shop_dic):
     return pet_shop_dic["admin"]["total_cash"]
 
-def add_or_remove_cash(pet_shop_dic, amount):
-    existing_cash = ["admin"],["total_cash"]
-    total_cash.add(amount)
-    return(total_cash)
+
+
+    
