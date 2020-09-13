afdff40d357e76b3d5b62abd330c05a8abe68b02
matinmac100 <matinmac100@yahoo.co.uk>
2020-09-13 12:36:55 +0100

    stuck on second

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -2,3 +2,5 @@
 def get_pet_shop_name(pet_shop_dictionary):
     return pet_shop_dictionary["name"]
 
+def get_total_cash(pet_shop_dictionary):
+    return pet_shop_dictionary["admin"]["total_cash"]
