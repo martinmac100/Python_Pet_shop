edd5291d31cc0215c69a1584149c92363a961ad4
matinmac100 <matinmac100@yahoo.co.uk>
2020-09-13 19:39:40 +0100

    no 2

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,6 +1,9 @@
 # WRITE YOUR FUNCTIONS HERE
-def get_pet_shop_name(pet_shop_dictionary):
-    return pet_shop_dictionary["name"]
+def get_pet_shop_name(pet_shop_dic):
+    return pet_shop_dic["name"]
 
-def get_total_cash(pet_shop_dictionary):
-    return pet_shop_dictionary["admin"]["total_cash"]
+def get_total_cash(pet_shop_dic):
+    return pet_shop_dic["admin"]["total_cash"]
+
+def add_or_remove_cash():
+    return
